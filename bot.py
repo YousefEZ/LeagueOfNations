e824332018fd93e897761f77cbfe8cd1419c4419
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2023-04-06 01:03:50 +0300

    :sparkles: add structure with the main bot

--- bot.py
+++ bot.py
@@ -1,13 +1,16 @@
 import os
 
 import discord
+import sqlalchemy.engine
 from discord.ext import commands
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
-cogs = tuple()
+cogs = "start",
+
 
 class LeagueOfNations(commands.AutoShardedBot):
-    def __init__(self):
+    def __init__(self, url: str):
         super().__init__(
             command_prefix="-",
             owner_id=251351879408287744,
@@ -15,8 +18,17 @@ class LeagueOfNations(commands.AutoShardedBot):
             case_insensitive=True,
             intents=discord.Intents.all()
         )
+        self._engine = create_engine(url, echo=True)
         self.remove_command('help')
         self.loop.create_task(self.ready())
+
+    @property
+    def connection(self) -> sqlalchemy.engine.Connection:
+        """Get an active connection to the database, or if one does not exist, then establish a connection
+
+        Returns (sqlalchemy.engine.Connection): An active connection to the database
+        """
+        return self._engine.connect()
 
     async def ready(self):
         await self.wait_until_ready()
@@ -33,5 +45,9 @@ class LeagueOfNations(commands.AutoShardedBot):
 if __name__ == "__main__":
     load_dotenv()
     TOKEN = os.getenv("DISCORD_TOKEN")
+    URL = os.getenv("DATABASE_URL")
+
     assert TOKEN is not None, "MISSING TOKEN IN .env FILE"
-    LeagueOfNations().run(TOKEN)
+    assert URL is not None, "MISSING DATABASE_URL IN .env FILE"
+
+    LeagueOfNations(URL).run(TOKEN)

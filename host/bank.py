abeba3c8d58c209b00bfb0e5f51147d225c52721
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2023-04-06 01:04:57 +0300

    :sparkles: added the structure for the host files

---
+++ host/bank.py
@@ -0,0 +1,56 @@
+from datetime import datetime
+from typing import TYPE_CHECKING, Optional
+
+import pint
+import sqlalchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
+
+if TYPE_CHECKING:
+    from host.player import Player
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class BankModel(Base):
+    __tablename__ = "Bank"
+
+    user_id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
+    treasury: Mapped[int] = mapped_column()
+    tax_rate: Mapped[int] = mapped_column()
+    last_accessed: Mapped[datetime] = mapped_column()
+
+
+class Bank:
+    __slots__ = "_identifier", "_player", "_engine", "_bank"
+
+    def __init__(self, identifier: int, player: Player, engine: sqlalchemy.Engine):
+        self._identifier: int = identifier
+        self._player: Player = player
+        self._engine: sqlalchemy.Engine = engine
+        with Session(self._engine) as session:
+            bank: Optional[BankModel] = session.query(BankModel).filter_by(user_id=self._identifier).first()
+            assert bank is not None, f"Bank does not exist for {self._identifier}"
+            self._bank: BankModel = bank
+
+    @property
+    def funds(self) -> int:
+        treasury = self._bank.treasury
+        with Session(self._engine) as session:
+            self._bank.treasury = treasury
+            session.commit()
+        return treasury
+
+    def _update_treasury(self) -> None:
+        with Session(self._engine) as session:
+            self._bank.treasury += self._retrieve_revenue(datetime.now()) - self._retrieve_expenses(datetime.now())
+            session.commit()
+
+    def _retrieve_revenue(self, timestamp: datetime) -> int:
+        time_difference = timestamp - self._bank.last_accessed
+        total_revenue = self._player.improvements.revenue * time_difference
+
+    def _retrieve_expenses(self, timestamp: datetime) -> int:
+        ...
---
+++ host/player.py
@@ -0,0 +1,55 @@
+from functools import cached_property
+
+import sqlalchemy
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, Session
+
+from host.bank import Bank
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class MetadataModel(Base):
+    __tablename__ = "metadata"
+
+    user_id: Mapped[int] = mapped_column(primary_key=True)
+    nation: Mapped[str] = mapped_column(candidate_key=True)
+
+
+
+class Metadata:
+    def __init__(self, identifier: int):
+        self._identifier = identifier
+        self._metadata = sqlalchemy.select(MetadataModel).where(MetadataModel.user_id == self._identifier)
+
+    @property
+    def nation(self) -> str:
+        return self._metadata.nation
+
+
+class Player:
+    def __init__(self, identifier: int):
+        self._identifier: int = identifier
+
+    @cached_property
+    def metadata(self) -> Metadata:
+        return Metadata(self._identifier)
+
+    @cached_property
+    def bank(self) -> Bank:
+        bank = Bank(self._identifier)
+        return bank
+
+    @property
+    def nation(self) -> str:
+        metadata = self.metadata
+        return metadata.nation
+
+    @staticmethod
+    def start(identifier: int, name: str, engine: sqlalchemy.Engine) -> None:
+        metadata = MetadataModel(user_id=identifier, nation=name)
+
+        with Session(engine) as session:
+            session.add(metadata)
+            session.commit()

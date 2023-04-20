9bbc18b86333d591b984c1f9c362ac60b93ede1f
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2023-04-21 01:22:07 +0300

    :sparkles: write outline of the start command

--- cogs/start.py
+++ cogs/start.py
@@ -1,39 +1,51 @@
-import sqlalchemy
+from typing import Literal
+
+import discord
+import qalib
+from discord import app_commands
 from discord.ext import commands
-
-from qalib import qalib_context, QalibContext
 from qalib.template_engines import formatter
+from sqlalchemy import Engine
 
-from host.player import Player
+from lon import LeagueOfNations
 
-NameTooLong = "name_too_long"
-NameTooShort = "name_too_short"
-NationStarted = "start"
+StartMessages = Literal["start"]
 
 
 class Start(commands.Cog):
-    """Cog that is handles the start command, and the creation of a new player"""
 
-    def __init__(self, bot, engine: sqlalchemy.Engine):
+    def __init__(self, bot: LeagueOfNations, engine: Engine):
         self.bot = bot
-        self._engine = engine
+        self.engine = engine
+
+    async def confirm_start(self, interaction: discord.Interaction):
+        connection = self.bot.connection
+        await interaction.response.send_message(":white_check_mark: Confirmed")
+
+    @staticmethod
+    async def decline_start(interaction: discord.Interaction):
+        await interaction.response.send_message(":x: Declined")
 
     @commands.command()
-    @qalib_context(formatter.Formatter(), "templates/start.xml")
-    async def start(self, ctx: QalibContext, *args):
-        name = ' '.join(args)
+    async def sync(self, ctx):
+        await ctx.bot.tree.sync(guid=ctx.guild)
+        await ctx.send("Synced")
 
-        if len(name) < 3:
-            await ctx.rendered_send(NameTooShort, keywords={"name": name})
-            return
+    @app_commands.command(name="start", description="Found Your Nation")
+    @app_commands.describe(nation_name="The name of your nation")
+    @qalib.qalib_interaction(formatter.Formatter(), "templates/start.xml")
+    async def start(self, ctx: qalib.QalibInteraction[StartMessages], nation_name: str) -> None:
+        """Slash Command that founds a nation with the nation_name if it does not exist
 
-        if len(name) > 75:
-            await ctx.rendered_send(NameTooLong, keywords={"name": name})
-            return
+        Args:
+            ctx (qalib.QalibInteraction[StartMessages]): The context of the interaction
+            nation_name (str): The name of the nation
+        """
 
-        Player.start(ctx.message.author.id, name, self._engine)
-        await ctx.rendered_send(NationStarted, keywords={"name": name})
+        await ctx.rendered_send("start",
+                                callables={"confirm": self.confirm_start, "decline": self.decline_start},
+                                keywords={"nation_name": nation_name})
 
 
-def setup(bot):
-    bot.add_cog(Start(bot, bot.database))
+async def setup(bot: LeagueOfNations) -> None:
+    await bot.add_cog(Start(bot, bot.engine))

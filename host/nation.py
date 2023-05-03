83c183a662b1cb07548bfdb7bf742ae4321d4f71
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2023-05-04 00:13:23 +0300

    :sparkles: introduce revenue

--- host/nation.py
+++ host/nation.py
@@ -1,28 +1,28 @@
 from __future__ import annotations
 
 from functools import cached_property
-from typing import get_args, List, Optional
+from typing import get_args, List
 
+from pint import Quantity
 from sqlalchemy import Engine
 from sqlalchemy.orm import Session
 
+from host import models, types
 from host.interior import Interior
-from host.types import Ministries, Happiness, UserId
 from host.ministry import Ministry
 from host.bank import Bank
-from host.models import MetadataModel
 from host.trade import Trade
 
 
 class Metadata:
-    def __init__(self, identifier: UserId, engine: Engine):
+    def __init__(self, identifier: types.UserId, engine: Engine):
         self._identifier = identifier
         self._engine = engine
 
     @cached_property
-    def metadata(self) -> MetadataModel:
+    def metadata(self) -> models.MetadataModel:
         with Session(self._engine) as session:
-            metadata: Optional[MetadataModel] = session.query(MetadataModel).filter_by(user_id=self._identifier).first()
+            metadata = session.query(models.MetadataModel).filter_by(user_id=self._identifier).first()
         if metadata is None:
             raise ValueError(f"Metadata does not exist for {self._identifier}")
         return metadata
@@ -33,13 +33,17 @@ class Metadata:
 
 
 class Nation:
-    def __init__(self, identifier: UserId, engine: Engine):
-        self._identifier: UserId = identifier
+    def __init__(self, identifier: types.UserId, engine: Engine):
+        self._identifier: types.UserId = identifier
         self._engine: Engine = engine
 
     @cached_property
-    def identifier(self) -> UserId:
+    def identifier(self) -> types.UserId:
         return self._identifier
+
+    @property
+    def name(self) -> str:
+        return self.metadata.nation_name
 
     @cached_property
     def metadata(self) -> Metadata:
@@ -47,22 +51,23 @@ class Nation:
 
     @cached_property
     def bank(self) -> Bank:
-        bank = Bank(self, self._engine)
-        return bank
+        return Bank(self, self._engine)
 
     @cached_property
     def trade(self) -> Trade:
-        trade = Trade(self, self._engine)
-        return trade
+        return Trade(self, self._engine)
 
-    @property
-    def nation_name(self) -> str:
-        metadata = self.metadata
-        return metadata.nation_name
+    @cached_property
+    def interior(self) -> Interior:
+        return Interior(self, self._engine)
+
+    @cached_property
+    def ministries(self) -> List[Ministry]:
+        return [getattr(self, ministry) for ministry in get_args(types.Ministries)]
 
     @classmethod
-    def start(cls, identifier: UserId, name: str, engine: Engine) -> Nation:
-        metadata = MetadataModel(user_id=identifier, nation=name)
+    def start(cls, identifier: types.UserId, name: str, engine: Engine) -> Nation:
+        metadata = models.MetadataModel(user_id=identifier, nation=name)
 
         with Session(engine) as session:
             session.add(metadata)
@@ -70,21 +75,13 @@ class Nation:
 
         return cls(identifier, engine)
 
-    def find_player(self, identifier: UserId) -> Nation:
+    def find_player(self, identifier: types.UserId) -> Nation:
         return Nation(identifier, self._engine)
 
-    @cached_property
-    def ministries(self) -> List[Ministry]:
-        return [getattr(self, ministry) for ministry in get_args(Ministries)]
-
-    @cached_property
-    def interior(self) -> Interior:
-        return Interior(self, self._engine)
-
     @property
-    def happiness(self) -> Happiness:
-        happiness: Happiness = sum((ministry.happiness for ministry in self.ministries), Happiness(0))
-        return Happiness(happiness * self.happiness_modifier)
+    def happiness(self) -> types.Happiness:
+        happiness: types.Happiness = sum(ministry.happiness for ministry in self.ministries)
+        return types.Happiness(happiness * self.happiness_modifier)
 
     @property
     def population(self) -> int:
@@ -92,4 +89,14 @@ class Nation:
 
     @property
     def happiness_modifier(self) -> float:
-        return 1.0 + sum(ministry.boost("happiness_modifier") for ministry in self.ministries)
+        return 1 + self.boost("happiness_modifier") / 100
+
+    def boost(self, boost: types.Boosts) -> float:
+        return sum(ministry.boost(boost) for ministry in self.ministries)
+
+    @property
+    @types.ureg.wraps(types.CurrencyRate, None)
+    def revenue(self) -> Quantity:
+        happiness = self.happiness
+        population = self.population
+        return happiness * 3 * population * types.CurrencyRate

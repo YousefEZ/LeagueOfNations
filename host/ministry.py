34b5210700f5f08dc467b830a9c81e45dab3e208
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2023-05-04 00:15:11 +0300

    :zap: use types namespace instead

--- host/ministry.py
+++ host/ministry.py
@@ -2,20 +2,25 @@ from __future__ import annotations
 
 from abc import ABC
 
-from host.types import ureg, CurrencyRate, Happiness, Boosts, default_boosts
+from host import types
 
 
 class Ministry(ABC):
 
     @property
-    @ureg.wraps(CurrencyRate, None)
-    def expenditure(self) -> CurrencyRate:
-        return 0 * CurrencyRate
+    @types.ureg.wraps(types.CurrencyRate, None)
+    def bill(self) -> types.CurrencyRate:
+        return 0 * types.CurrencyRate
 
     @property
-    def happiness(self) -> Happiness:
-        return Happiness(0)
+    @types.ureg.wraps(types.CurrencyRate, None)
+    def revenue(self) -> types.CurrencyRate:
+        return 0 * types.CurrencyRate
+
+    @property
+    def happiness(self) -> types.Happiness:
+        return types.Happiness(0)
 
     # noinspection PyMethodMayBeStatic
-    def boost(self, boost: Boosts) -> float:
-        return default_boosts[boost]
+    def boost(self, boost: types.Boosts) -> float:
+        return types.default_boosts[boost]

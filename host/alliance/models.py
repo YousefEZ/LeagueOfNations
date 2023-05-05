ef4fa4e4a3d4ff1f0273a012aa2b6bfc26ab68b2
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2023-05-05 20:36:05 +0300

    :sparkles: alliance models

---
+++ host/alliance/models.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from sqlalchemy.orm import mapped_column, Mapped
+
+from host import base_types
+from host.alliance import types
+from host.base_models import Base
+
+
+class AllianceMetaModel(Base):
+    __tablename__ = "AllianceMeta"
+
+    id: Mapped[types.AllianceId] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    flag: Mapped[str]
+    date_founded: Mapped[datetime]
+    description: Mapped[str]
+
+
+class AllianceBankModel(Base):
+    __tablename__ = "AllianceBank"
+
+    id: Mapped[types.AllianceId] = mapped_column(primary_key=True)
+    bank_name: Mapped[str] = mapped_column(primary_key=True)
+    treasury: Mapped[int]
+
+
+class AllianceMemberModel(Base):
+    __tablename__ = "AllianceMember"
+
+    id: Mapped[types.AllianceId] = mapped_column(primary_key=True)
+    user_id: Mapped[base_types.UserId] = mapped_column(primary_key=True)
+    role: Mapped[types.AllianceRoles]
+    date_joined: Mapped[datetime]

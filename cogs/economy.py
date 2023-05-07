9998b133d0d1d7d611e09f274cf57f6031bed87d
Yousef Zaher <45167695+YousefEZ@users.noreply.github.com>
2023-05-07 19:03:43 +0100

    :zap: use custom Jinja2 environment

--- cogs/economy.py
+++ cogs/economy.py
@@ -1,10 +1,11 @@
 from typing import Literal
 
+import qalib
 from discord import app_commands
 from discord.ext import commands
-from qalib.template_engines import formatter
-import qalib
+from qalib.template_engines.jinja2 import Jinja2
 
+from cogs.custom_jinja2 import ENVIRONMENT
 from lon import LeagueOfNations
 
 EconomyMessages = Literal["balance"]
@@ -15,13 +16,8 @@ class Economy(commands.Cog):
     def __init__(self, bot: LeagueOfNations):
         self.bot = bot
 
-    @commands.command()
-    async def sync(self, ctx):
-        await ctx.bot.tree.sync(guid=ctx.guild)
-        await ctx.send("Synced")
-
     @app_commands.command(name="balance", description="Check the balance of you Nation")
-    @qalib.qalib_interaction(formatter.Formatter(), "templates/balance.xml")
+    @qalib.qalib_interaction(Jinja2(ENVIRONMENT), "templates/economy.xml")
     async def balance(self, ctx: qalib.QalibInteraction[EconomyMessages]) -> None:
         """Balance command that shows the balance of the nations funds
 
@@ -30,3 +26,7 @@ class Economy(commands.Cog):
         """
         nation = self.bot.get_nation(ctx.user.id)
         await ctx.rendered_send("balance", keywords={"nation": nation})
+
+
+async def setup(bot: LeagueOfNations) -> None:
+    await bot.add_cog(Economy(bot))
--- cogs/start.py
+++ cogs/start.py
@@ -5,8 +5,11 @@ import qalib
 from discord import app_commands
 from discord.ext import commands
 from qalib.template_engines import formatter
-from sqlalchemy.engine import Engine
+from qalib.template_engines.jinja2 import Jinja2
 
+from cogs.custom_jinja2 import ENVIRONMENT
+from host.base_types import UserId
+from host.nation import Nation
 from lon import LeagueOfNations
 
 StartMessages = Literal["start"]
@@ -14,26 +17,19 @@ StartMessages = Literal["start"]
 
 class Start(commands.Cog):
 
-    def __init__(self, bot: LeagueOfNations, engine: Engine):
+    def __init__(self, bot: LeagueOfNations):
         self.bot = bot
-        self.engine = engine
 
     async def confirm_start(self, interaction: discord.Interaction):
-        connection = self.bot.connection
         await interaction.response.send_message(":white_check_mark: Confirmed")
 
     @staticmethod
     async def decline_start(interaction: discord.Interaction):
         await interaction.response.send_message(":x: Declined")
 
-    @commands.command()
-    async def sync(self, ctx):
-        await ctx.bot.tree.sync(guid=ctx.guild)
-        await ctx.send("Synced")
-
     @app_commands.command(name="start", description="Found Your Nation")
     @app_commands.describe(nation_name="The name of your nation")
-    @qalib.qalib_interaction(formatter.Formatter(), "templates/start.xml")
+    @qalib.qalib_interaction(Jinja2(ENVIRONMENT), "templates/start.xml")
     async def start(self, ctx: qalib.QalibInteraction[StartMessages], nation_name: str) -> None:
         """Slash Command that founds a nation with the nation_name if it does not exist
 
@@ -41,6 +37,7 @@ class Start(commands.Cog):
             ctx (qalib.QalibInteraction[StartMessages]): The context of the interaction
             nation_name (str): The name of the nation
         """
+        Nation.start(UserId(ctx.user.id), nation_name, self.bot.engine)
 
         await ctx.rendered_send("start",
                                 callables={"confirm": self.confirm_start, "decline": self.decline_start},
@@ -48,4 +45,4 @@ class Start(commands.Cog):
 
 
 async def setup(bot: LeagueOfNations) -> None:
-    await bot.add_cog(Start(bot, bot.engine))
+    await bot.add_cog(Start(bot))

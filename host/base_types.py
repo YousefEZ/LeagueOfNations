103ee25ad41aed3f0cd65c2c2793f83f452cc04d
yzahe <45167695+YousefEZ@users.noreply.github.com>
2023-07-22 21:34:54 +0100

    :truck: move registry

--- host/base_types.py
+++ host/base_types.py
@@ -4,17 +4,18 @@ from typing import NewType
 
 from babel import numbers
 
-from host.currency import ureg, Currency, CurrencyRate
+from host.currency import Currency, CurrencyRate
+from host.ureg import Registry
 
 UserId = NewType("UserId", int)
 
 
-@ureg.wraps(None, Currency)
+@Registry.wraps(None, Currency)
 def render_currency(value: Currency) -> str:
     return numbers.format_compact_currency(value, currency="USD", locale="en_US", fraction_digits=3)
 
 
-@ureg.wraps(None, CurrencyRate)
+@Registry.wraps(None, CurrencyRate)
 def render_currency_rate(value: Currency) -> str:
     magnitude = numbers.format_compact_currency(value, currency="USD", locale="en_US", fraction_digits=3)
     return f"{magnitude} / day"

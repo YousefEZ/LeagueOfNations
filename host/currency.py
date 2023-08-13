18521b8e00f24e696367df205726518eb299a209
yzahe <45167695+YousefEZ@users.noreply.github.com>
2023-08-13 18:58:06 +0100

    :sparkles: add lnd and lnd_rate functions to convert into unit

--- host/currency.py
+++ host/currency.py
@@ -5,3 +5,13 @@ from host.ureg import Registry
 Registry.define("LND = [currency]")
 Currency = Registry.LND
 CurrencyRate = Registry.LND / Registry.days
+
+
+@Registry.wraps(Currency, None)
+def lnd(value: Registry.LND) -> Currency:
+    return value * Currency
+
+
+@Registry.wraps(CurrencyRate, None)
+def lnd_rate(value: Registry.LND / Registry.days) -> CurrencyRate:
+    return value * CurrencyRate

cacc1e1bcae48f066c22bc7f7f25bdc40c5174c1
yzahe <45167695+YousefEZ@users.noreply.github.com>
2023-08-14 00:18:22 +0100

    :zap: improve model for units

--- host/nation/interior.py
+++ host/nation/interior.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from functools import cached_property
-from typing import TYPE_CHECKING, Literal, TypeVar, Generic
+from typing import TYPE_CHECKING, Literal, TypeVar, Protocol, Type
 
 from sqlalchemy import Engine
 from sqlalchemy.orm import Session
@@ -12,7 +12,7 @@ from host.defaults import defaults
 from host.gameplay_settings import GameplaySettings
 from host.nation import models
 from host.nation.ministry import Ministry
-from host.nation.types.basic import Population, InfrastructureUnit, LandUnit, TechnologyUnit
+from host.nation.types.basic import Population
 from host.nation.types.interior import Data, InfrastructurePoints, TechnologyPoints, LandPoints, PurchaseResult, \
     SellResult
 
@@ -30,101 +30,126 @@ BuyMessages = Literal[
 
 K = TypeVar("K")
 
-CashbackModifier = 0.5
 
-
-class UnitExchange(Generic[K]):
-    __slots__ = "_singleton", "_interior", "_nation", "_engine"
-    unit: Data[K]
+class UnitExchangeProtocol(Protocol[K]):
 
     def __init__(self, nation: Nation, interior: models.InteriorModel, engine: Engine):
-        self._nation = nation
-        self._interior = interior
-        self._engine = engine
-
-    @host.ureg.Registry.wraps(currency.Currency, [None, None, None])
-    def _get_unit_price_at(self, point: K, level: K) -> currency.Currency:
-        return self.unit.PricePoints[point] * level + self.unit.FloorPrice
-
-    @host.ureg.Registry.wraps(currency.Currency, [None, None])
-    def price_at(self, level: K) -> currency.Currency:
-        previous_point = 0
-        for point, price in self.unit.PricePoints.items():
-            if level <= point:
-                return self._get_unit_price_at(previous_point, level) * self.price_modifier
-            previous_point = point
-        return self._get_unit_price_at(previous_point, level) * self.price_modifier
-
-    @host.ureg.Registry.wraps(currency.Currency, [None, None])
-    def price_order(self, amount: K) -> currency.Currency:
-        return sum((self.price_at(self.amount + i) for i in range(amount)), currency.Currency(0)) * self.price_modifier
-
-    @host.ureg.Registry.wraps(currency.CurrencyRate, [None, None, None])
-    def _get_unit_bill_at(self, point: K, level: K) -> currency.CurrencyRate:
-        return self.unit.BillPoints[point] * level
-
-    @host.ureg.Registry.wraps(currency.CurrencyRate, [None, None])
-    def bill_at(self, level: K) -> currency.CurrencyRate:
-        previous_point = 0
-        for point, price in self.unit.PricePoints.items():
-            if level <= point:
-                return self._get_unit_bill_at(previous_point, level)
-            previous_point = point
-        return self._get_unit_bill_at(previous_point, level)
+        ...
 
     @property
     def amount(self) -> K:
-        return self.unit.get(self._interior)
+        raise NotImplementedError
 
-    def _set_amount(self, value: K) -> None:
-        with Session(self._engine) as session:
-            self.unit.set(self._interior, value)
-            session.add(self._interior)
-            session.commit()
+    def price_at(self, level: K) -> currency.Currency:
+        raise NotImplementedError
 
-    @property
-    def price_modifier(self) -> float:
-        return 1 - getattr(self._nation.boost, self.unit.PriceModifier)
+    def price_order(self, amount: K) -> currency.Currency:
+        raise NotImplementedError
 
-    @property
-    def bill_modifier(self) -> float:
-        return 1 - getattr(self._nation.boost, self.unit.BillModifier)
-
-    def buy(self, amount: K) -> PurchaseResult:
-        if amount <= 0:
-            return PurchaseResult.NEGATIVE_AMOUNT
-        price = self.price_order(amount)
-        if self._nation.bank.enough_funds(price):
-            return PurchaseResult.INSUFFICIENT_FUNDS
-        self._nation.bank.deduct(price)
-        self._set_amount(self.amount + amount)
-        return PurchaseResult.SUCCESS
-
-    def sell(self, amount: K) -> SellResult:
-        if amount <= 0:
-            return SellResult.NEGATIVE_AMOUNT
-        if self.amount < amount:
-            return SellResult.INSUFFICIENT_AMOUNT
-        self._nation.bank.add(self.price_order(amount) * defaults.interior.cashback_modifier)
-        self._set_amount(self.amount - amount)
-        return SellResult.SUCCESS
+    def bill_at(self, level: K) -> currency.CurrencyRate:
+        raise NotImplementedError
 
     @property
     @host.ureg.Registry.wraps(currency.CurrencyRate, None)
     def bill(self) -> currency.CurrencyRate:
-        return self.bill_at(self.amount) * self.bill_modifier
+        raise NotImplementedError
+
+    def buy(self, amount: K) -> PurchaseResult:
+        raise NotImplementedError
+
+    def sell(self, amount: K) -> SellResult:
+        raise NotImplementedError
 
 
-class Infrastructure(UnitExchange[InfrastructureUnit]):
-    unit = InfrastructurePoints
+def unit_exchange(cls: Type[Data[K]]) -> Type[UnitExchangeProtocol[K]]:
+    class UnitExchange(UnitExchangeProtocol[K]):
+        __slots__ = "_singleton", "_interior", "_nation", "_engine"
+        unit: Type[Data[K]] = cls
+
+        def __init__(self, nation: Nation, interior: models.InteriorModel, engine: Engine):
+            self._nation = nation
+            self._interior = interior
+            self._engine = engine
+
+        @host.ureg.Registry.wraps(currency.Currency, [None, None, None])
+        def _get_unit_price_at(self, point: K, level: K) -> currency.Currency:
+            return self.unit.PricePoints[point] * level + self.unit.FloorPrice
+
+        @host.ureg.Registry.wraps(currency.Currency, [None, None])
+        def price_at(self, level: K) -> currency.Currency:
+            previous_point = 0
+            for point, price in self.unit.PricePoints.items():
+                if level <= point:
+                    return self._get_unit_price_at(previous_point, level) * self.price_modifier
+                previous_point = point
+            return self._get_unit_price_at(previous_point, level) * self.price_modifier
+
+        @host.ureg.Registry.wraps(currency.Currency, [None, None])
+        def price_order(self, amount: K) -> currency.Currency:
+            return sum((self.price_at(self.amount + i) for i in range(amount)),
+                       currency.Currency(0)) * self.price_modifier
+
+        @host.ureg.Registry.wraps(currency.CurrencyRate, [None, None, None])
+        def _get_unit_bill_at(self, point: K, level: K) -> currency.CurrencyRate:
+            return self.unit.BillPoints[point] * level
+
+        @host.ureg.Registry.wraps(currency.CurrencyRate, [None, None])
+        def bill_at(self, level: K) -> currency.CurrencyRate:
+            previous_point = 0
+            for point, price in self.unit.PricePoints.items():
+                if level <= point:
+                    return self._get_unit_bill_at(previous_point, level)
+                previous_point = point
+            return self._get_unit_bill_at(previous_point, level)
+
+        @property
+        def amount(self) -> K:
+            return self.unit.get(self._interior)
+
+        def _set_amount(self, value: K) -> None:
+            with Session(self._engine) as session:
+                self.unit.set(self._interior, value)
+                session.add(self._interior)
+                session.commit()
+
+        @property
+        def price_modifier(self) -> float:
+            return 1 - getattr(self._nation.boost, self.unit.PriceModifier)
+
+        @property
+        def bill_modifier(self) -> float:
+            return 1 - getattr(self._nation.boost, self.unit.BillModifier)
+
+        def buy(self, amount: K) -> PurchaseResult:
+            if amount <= 0:
+                return PurchaseResult.NEGATIVE_AMOUNT
+            price = self.price_order(amount)
+            if self._nation.bank.enough_funds(price):
+                return PurchaseResult.INSUFFICIENT_FUNDS
+            self._nation.bank.deduct(price)
+            self._set_amount(self.amount + amount)
+            return PurchaseResult.SUCCESS
+
+        def sell(self, amount: K) -> SellResult:
+            if amount <= 0:
+                return SellResult.NEGATIVE_AMOUNT
+            if self.amount < amount:
+                return SellResult.INSUFFICIENT_AMOUNT
+            self._nation.bank.add(self.price_order(amount) * defaults.interior.cashback_modifier)
+            self._set_amount(self.amount - amount)
+            return SellResult.SUCCESS
+
+        @property
+        @host.ureg.Registry.wraps(currency.CurrencyRate, None)
+        def bill(self) -> currency.CurrencyRate:
+            return self.bill_at(self.amount) * self.bill_modifier
+
+    return UnitExchange
 
 
-class Technology(UnitExchange[TechnologyUnit]):
-    unit = TechnologyPoints
-
-
-class Land(UnitExchange[LandUnit]):
-    unit = LandPoints
+Infrastructure = unit_exchange(InfrastructurePoints)
+Technology = unit_exchange(TechnologyPoints)
+Land = unit_exchange(LandPoints)
 
 
 class Interior(Ministry):
@@ -151,9 +176,9 @@ class Interior(Ministry):
     @property
     @host.ureg.Registry.wraps(currency.CurrencyRate, None)
     def revenue(self) -> currency.CurrencyRate:
-        income_increase = 1 + self._player.boost.income_increase
-        return self._player.interior.population * GameplaySettings["interior"][
-            "revenue_per_population"] * income_increase
+        gdb_per_capita = GameplaySettings.interior.revenue_per_population + self._player.boost.income_increase
+        income_modifier = 1 + self._player.boost.income_modifier
+        return self.population * gdb_per_capita * income_modifier
 
     @cached_property
     def infrastructure(self) -> Infrastructure:

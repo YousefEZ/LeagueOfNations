0d8d621916219e6545334d3b9c5429fa6f004d51
yzahe <45167695+YousefEZ@users.noreply.github.com>
2023-08-13 18:59:27 +0100

    :sparkles: add function that converts value to land unit

--- host/nation/types/basic.py
+++ host/nation/types/basic.py
@@ -7,3 +7,8 @@ Population = NewType("Population", int)
 InfrastructureUnit = NewType("InfrastructureUnit", int)
 TechnologyUnit = NewType("TechnologyUnit", int)
 LandUnit = ureg.Registry.kilometer ** 2
+
+
+@ureg.Registry.wraps(LandUnit, None)
+def land(value: ureg.Registry.kilometer ** 2) -> LandUnit:
+    return value * LandUnit

8610418a8ed463682468b648aa203e42bab4ca4a
yzahe <45167695+YousefEZ@users.noreply.github.com>
2023-08-21 00:54:32 +0100

    :sparkles: add technology and land commands

--- view/cogs/economy.py
+++ view/cogs/economy.py
@@ -1,5 +1,4 @@
-import traceback
-from typing import Literal
+from typing import Literal, Dict, Callable, Coroutine
 
 import discord
 import qalib
@@ -8,22 +7,49 @@ from discord.ext import commands
 from qalib.template_engines.jinja2 import Jinja2
 
 from host.nation import Nation
-from host.nation.interior import UnitExchangeProtocol
-from host.nation.types.interior import PurchaseResult
+from host.nation.interior import UnitExchangeProtocol, K
+from host.nation.types.interior import PurchaseResult, SellResult
 from lon import LeagueOfNations
 from view.cogs.custom_jinja2 import ENVIRONMENT
 
+UnitActions = Literal["buy", "sell"]
+PositiveInteger = discord.app_commands.Range[int, 1]
+
 InfrastructureMessages = Literal["cost", "insufficient_funds", "success"]
 EconomyMessages = Literal["balance", InfrastructureMessages]
-UnitActions = Literal["buy", "sell"]
+PurchaseMessages = Literal["cost", "insufficient_funds", "success"]
+SellMessages = Literal["cashback", "insufficient_amount", "success"]
 
-PositiveInteger = discord.app_commands.Range[int, 1]
+purchase_mappings: Dict[PurchaseResult, PurchaseMessages] = {
+    PurchaseResult.INSUFFICIENT_FUNDS: "insufficient_funds",
+    PurchaseResult.SUCCESS: "success",
+}
+
+sell_mappings: Dict[SellResult, PurchaseMessages] = {
+    SellResult.INSUFFICIENT_AMOUNT: "insufficient_amount",
+    SellResult.SUCCESS: "success",
+}
+
+UnitTypes = Literal["infrastructure", "land", "technology"]
+unit_exchange_mappings: Dict[UnitTypes, Callable[[Nation], UnitExchangeProtocol]] = {
+    "infrastructure": lambda nation: nation.infrastructure,
+    "land": lambda nation: nation.land,
+    "technology": lambda nation: nation.technology,
+}
+
+ExchangeMessages = Literal[PurchaseMessages, SellMessages]
 
 
 class Economy(commands.Cog):
 
     def __init__(self, bot: LeagueOfNations):
         self.bot = bot
+        self._unit_actions_mapping: Dict[UnitActions, Callable[
+            [qalib.QalibInteraction[ExchangeMessages], Nation, UnitExchangeProtocol[K], PositiveInteger], Coroutine[
+                None, None, None]]] = {
+            "buy": self.buy,
+            "sell": self.sell
+        }
 
     @app_commands.command(name="balance", description="Check the balance of you Nation")
     @qalib.qalib_interaction(Jinja2(ENVIRONMENT), "templates/economy.xml")
@@ -36,18 +62,18 @@ class Economy(commands.Cog):
         nation = self.bot.get_nation(ctx.user.id)
         await ctx.rendered_send("balance", keywords={"nation": nation})
 
+    @staticmethod
     async def buy(
-            self,
-            ctx: qalib.QalibInteraction[EconomyMessages],
+            ctx: qalib.QalibInteraction[PurchaseMessages],
             nation: Nation,
-            unit: UnitExchangeProtocol,
+            unit: UnitExchangeProtocol[K],
             amount: PositiveInteger
     ) -> None:
         """Buy command that buys an item
 
         Args:
             ctx (qalib.QalibInteraction[EconomyMessages]): The context of the interaction
-            nation (Nation): the nation that is buying the unit
+            nation (Nation): the nation that is buying the units
             unit (UnitExchangeProtocol): The unit to buy
             amount (int): The amount to buy
         """
@@ -56,45 +82,94 @@ class Economy(commands.Cog):
 
         async def buy_confirmation(interaction: discord.Interaction) -> None:
             await interaction.response.defer()
-            try:
-                result = unit.buy(amount)
-                if result == PurchaseResult.INSUFFICIENT_FUNDS:
-                    await ctx.display("insufficient_funds",
-                                      keywords={"nation": nation, "amount": amount, "price": price})
-                elif result == PurchaseResult.SUCCESS:
-                    await ctx.display("success", keywords={"nation": nation, "amount": amount, "price": price})
-            except Exception as e:
-                print(e)
-                print(traceback.format_exc())
+            await ctx.display(purchase_mappings[unit.buy(amount)],
+                              keywords={"nation": nation, "amount": amount, "price": price})
 
         await ctx.display("cost",
                           keywords={"nation": nation, 'amount': amount, 'price': price},
                           callables={"confirm": buy_confirmation})
 
+    @staticmethod
+    async def sell(
+            ctx: qalib.QalibInteraction[SellMessages],
+            nation: Nation,
+            unit: UnitExchangeProtocol,
+            amount: PositiveInteger
+    ) -> None:
+        """Sell command that sells an item
+
+        Args:
+            ctx (qalib.QalibInteraction[EconomyMessages]): The context of the interaction
+            nation (Nation): the nation that is selling the units
+            unit (UnitExchangeProtocol): The unit to sell
+            amount (int): The amount to sell
+        """
+
+        units = unit.amount
+        cashback = unit.price_order(amount)
+
+        async def sell_confirmation(interaction: discord.Interaction) -> None:
+            await interaction.response.defer()
+            await ctx.display(sell_mappings[unit.sell(amount)],
+                              keywords={"nation": nation, "amount": amount, "cashback": cashback, "units": units})
+
+        await ctx.display("cashback",
+                          keywords={"nation": nation, 'amount': amount, 'cashback': cashback},
+                          callables={"confirm": sell_confirmation})
+
     @app_commands.command(name="infrastructure", description="handling infrastructure of the nation")
     @qalib.qalib_interaction(Jinja2(ENVIRONMENT), "templates/infrastructure.xml")
     async def infrastructure(
             self,
-            ctx: qalib.QalibInteraction[EconomyMessages],
+            ctx: qalib.QalibInteraction[ExchangeMessages],
             action: UnitActions,
             amount: PositiveInteger
     ) -> None:
         """Infrastructure command that shows the infrastructure of the nation
 
         Args:
-            ctx (qalib.QalibInteraction[EconomyMessages]): The context of the interaction
+            ctx (qalib.QalibInteraction[ExchangeMessages]): The context of the interaction
             action (UnitActions): The action to perform
             amount (int): The amount to perform the action on. Defaults to None.
         """
         nation = self.bot.get_nation(ctx.user.id)
-        if action == "buy":
-            await self.buy(ctx, nation, nation.interior.infrastructure, amount)
-        elif action == "sell":
-            result = nation.interior.infrastructure.sell(amount)
-            if result == PurchaseResult.INSUFFICIENT_FUNDS:
-                await ctx.rendered_send("insufficient_amount", keywords={"nation": nation})
-            elif result == PurchaseResult.SUCCESS:
-                await ctx.rendered_send("success", keywords={"nation": nation})
+        await self._unit_actions_mapping[action](ctx, nation, nation.interior.infrastructure, amount)
+
+    @app_commands.command(name="technology", description="handling technology of the nation")
+    @qalib.qalib_interaction(Jinja2(ENVIRONMENT), "templates/technology.xml")
+    async def technology(
+            self,
+            ctx: qalib.QalibInteraction[ExchangeMessages],
+            action: UnitActions,
+            amount: PositiveInteger
+    ) -> None:
+        """Technology command that shows the technology of the nation
+
+        Args:
+            ctx (qalib.QalibInteraction[ExchangeMessages]): The context of the interaction
+            action (UnitActions): The action to perform
+            amount (int): The amount to perform the action on. Defaults to None.
+        """
+        nation = self.bot.get_nation(ctx.user.id)
+        await self._unit_actions_mapping[action](ctx, nation, nation.interior.technology, amount)
+
+    @app_commands.command(name="land", description="handling land of the nation")
+    @qalib.qalib_interaction(Jinja2(ENVIRONMENT), "templates/land.xml")
+    async def land(
+            self,
+            ctx: qalib.QalibInteraction[ExchangeMessages],
+            action: UnitActions,
+            amount: PositiveInteger
+    ) -> None:
+        """Land command that shows the land of the nation
+
+        Args:
+            ctx (qalib.QalibInteraction[ExchangeMessages]): The context of the interaction
+            action (UnitActions): The action to perform
+            amount (int): The amount to perform the action on. Defaults to None.
+        """
+        nation = self.bot.get_nation(ctx.user.id)
+        await self._unit_actions_mapping[action](ctx, nation, nation.interior.land, amount)
 
 
 async def setup(bot: LeagueOfNations) -> None:

30905469713f700a8e7122c16da9c92ab3b743f1
yzahe <45167695+YousefEZ@users.noreply.github.com>
2023-08-24 01:13:57 +0100

    :pencil2: rename Improvement to ImprovementSchema

--- host/nation/types/improvements.py
+++ host/nation/types/improvements.py
@@ -4,13 +4,13 @@ import json
 from typing import List
 
 import pydantic
-from pydantic import BaseModel, computed_field
+from pydantic import BaseModel
 
 from host import currency
 from host.nation.types.boosts import BoostsLookup
 
 
-class Improvement(BaseModel, frozen=True):
+class ImprovementSchema(BaseModel, frozen=True):
     name: str
     emoji: str
     description: str
@@ -25,5 +25,5 @@ class Improvement(BaseModel, frozen=True):
 
 
 with open("objects/improvements.json", encoding="utf8", mode="r") as improvements_file:
-    Improvements = {identifier: Improvement.model_validate(improvement) for identifier, improvement in
+    Improvements = {identifier: ImprovementSchema.model_validate(improvement) for identifier, improvement in
                     json.load(improvements_file).items()}

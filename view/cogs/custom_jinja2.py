ff759198ee75ae79096e55ac78d43201addfdeda
yzahe <45167695+YousefEZ@users.noreply.github.com>
2023-08-27 00:53:34 +0100

    :wrench: add autoescape to configuration of Env

--- view/cogs/custom_jinja2.py
+++ view/cogs/custom_jinja2.py
@@ -3,6 +3,6 @@ from jinja2.environment import Environment
 
 from host.base_types import render_currency, render_currency_rate
 
-ENVIRONMENT = Environment(loader=FileSystemLoader("templates/"))
+ENVIRONMENT = Environment(loader=FileSystemLoader("templates/"), autoescape=True)
 ENVIRONMENT.filters["currency"] = render_currency
 ENVIRONMENT.filters["currency_rate"] = render_currency_rate

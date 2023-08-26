832149533827e4daf0af57aa6a92b39225a59376
yzahe <45167695+YousefEZ@users.noreply.github.com>
2023-08-27 00:52:38 +0100

    :zap: make dependencies a dict that takes improv name and quantity

--- host/nation/types/improvements.py
+++ host/nation/types/improvements.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import json
-from typing import List
+from typing import List, Dict
 
 import pydantic
 from pydantic import BaseModel
@@ -14,10 +14,11 @@ class ImprovementSchema(BaseModel, frozen=True):
     name: str
     emoji: str
     description: str
+    url: str
     limit: int
     raw_price: int = pydantic.Field(alias="price")
     boosts: BoostsLookup
-    dependencies: List[str]
+    dependencies: Dict[str, int]
 
     @property
     def price(self) -> currency.Currency:

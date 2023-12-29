050737bf53de42c556ed31c60de15a37de2a3dbd
YousefEZ <45167695+YousefEZ@users.noreply.github.com>
2023-12-29 23:46:14 +0200

    :bug: missing variables will have their attributes reference themselves

--- view/cogs/custom_jinja2.py
+++ view/cogs/custom_jinja2.py
@@ -1,8 +1,9 @@
-from jinja2 import BaseLoader, FileSystemLoader
+from jinja2 import FileSystemLoader, ChainableUndefined
 from jinja2.environment import Environment
 
 from host.base_types import render_currency, render_currency_rate
 
-ENVIRONMENT = Environment(loader=FileSystemLoader("templates/"), autoescape=True)
+
+ENVIRONMENT = Environment(loader=FileSystemLoader("templates/"), autoescape=True, undefined=ChainableUndefined)
 ENVIRONMENT.filters["currency"] = render_currency
 ENVIRONMENT.filters["currency_rate"] = render_currency_rate
